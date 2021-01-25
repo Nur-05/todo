ae051aefb75601d4fd5e4099ae3f4ae1f1955368
Nurkyz <nurkyz.rashitova@gmail.com>
2021-01-25 14:28:14 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo
+
+
+def homepage(request):
+    return render(request, "index.html")
+
+
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
+
+
+def second(request):
+    return HttpResponse("test 2 page")
+
+def str1(request):
+    return HttpResponse("Ваша запись была добавлена успешно")
+
+def str2(request):
+    return HttpResponse("Ваша запись была изменена успешно")
+
+def str3(request):
+    return HttpResponse("Ваша запись была удалена")
+
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
